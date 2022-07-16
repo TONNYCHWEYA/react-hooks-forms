123b06488d311376030a8dae5c64c7c23a38c17b
TONNYCHWEYA <chweyatonny@gmail.com>
2022-07-16 11:14:14 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ParentComponent from './components/ParentComponent';
 import Form from './components/Form'
 
 ReactDOM.render(
-  <Form />,
+  <ParentComponent />,
   document.getElementById('root')
 );
